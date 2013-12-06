2c2094135ea5a2b49c707a22f09e92c928a1e917
tjbp <tom@tjbp.net>
2013-12-06 15:09:09 +0000

    Added bash script for updating, updated hgignore

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+curl -sS https://getcomposer.org/installer | php
+php composer.phar create-project composer/satis --stability=dev --keep-vcs
+php satis/bin/satis build satis.json public
+rm composer.phar
+rm -rf satis
