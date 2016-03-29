7acd1fa52b47c0d60e78913e87152f63cee6bea4
tjbp <tom@tjbp.net>
2016-03-29 23:04:37 +0100

    Fixed shebang

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 curl -sS https://getcomposer.org/installer | php
 php composer.phar create-project composer/satis --stability=dev --keep-vcs
